1ea4f748cb39bb6da3e4d291ef103853e8f1dc66
Jorge Luis Vasquez <jlv0362@gmail.com>
2025-06-17 09:46:59 -0300

    Primer push, API1, Procesamiento Datos

---
+++ API-1-ProcesamientoDatos.py
@@ -0,0 +1,13 @@
+# Importar bibliotecas pandas y numpy
+import pandas as pd
+import numpy as np
+
+# Crear los array A y B, segun consigna usando numpy
+a=np.array([[1,2,3],[4,5,6]])
+b=np.array([[10,11,12],[12,14,15]])
+
+# Sumar los arreglos a y b
+suma=np.add(a,b)
+print("\nSalida suma de array A y B, utilizando numpy\n")
+print(suma)
+print("\n-------------------------------------------")
